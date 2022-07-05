e9226453f17a4c30be52cc19a335f45d24b822e6
shubham <shubhamdesai77@gmail.com>
2022-07-05 16:42:01 +0100

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,15 @@
+#from _typeshed import Self
+from io import DEFAULT_BUFFER_SIZE
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=122)
+    email = models.CharField(max_length=122)
+    phone = models.CharField(max_length=12)
+    desc = models.TextField()
+    date = models.DateField()
+
+
+
+    
